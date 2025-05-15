3047934d435d3143ab5bb074fd4c7deba005987f
Kevin Koehler <kevin.koehler@santannapisa.it>
2025-05-15 15:45:14 +0200

    Update Replication_code.R

--- Replication_code.R
+++ Replication_code.R
@@ -1,1 +1,28 @@
+library(tidyverse)
+library(haven)
+library(stargazer)
+library(psych)
+library(knitr)
+library(kableExtra)
 
+data <- read_dta("data_2011-07-26.dta")
+
+sumstats <- data %>%
+  select(-country, -iso3166) %>%
+  describe() %>%
+  select(n, mean, sd, min, max) %>%
+  mutate(mean=round(mean,2),
+         sd=round(sd,2),
+         min=round(min,2),
+         max=round(max,2))
+
+summary_table <- kable(sumstats, 
+      format = "html", 
+      longtable = TRUE, 
+      booktabs = TRUE,
+      caption = "Summary statistics", label = "tab:sum_stats") %>%
+  kable_styling(latex_options = c("repeat_header", 
+                                  "striped",
+                                  "condensed"))
+
+write_lines(summary_table, "summary_table.html")
