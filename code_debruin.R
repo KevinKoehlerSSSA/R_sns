baf612c846ae22837197448abc2729bd123d2096
Kevin Koehler <kevin.koehler@santannapisa.it>
2025-05-16 16:21:44 +0200

    Update code_debruin.R

--- code_debruin.R
+++ code_debruin.R
@@ -46,3 +46,32 @@ kable(descriptives,
                     "Total")) %>%
   add_header_above(c(" ","Number of Counterweights"=4,"New Counterweight"=2,"")) 
 
+model1 <- glm(success~
+                counterbalancing,
+              data=data,
+              family="binomial")
+clustered_se1 <- sqrt(diag(vcovCL(model1, cluster = ~ ccode)))
+
+model2 <- glm(success~
+                counterbalancing +
+                top +
+                middle,
+              data=data,
+              family="binomial")
+clustered_se2 <- sqrt(diag(vcovCL(model2, cluster = ~ ccode)))
+
+model3 <- glm(success~
+                counterbalancing +
+                top +
+                middle +
+                military +
+                dem7 +
+                lngdppc +
+                chgdp +
+                recent3 +
+                regional3 +
+                recent_rev +
+                coldwar,
+              data=data,
+              family="binomial")
+clustered_se3 <- sqrt(diag(vcovCL(model3, cluster = ~ ccode)))
