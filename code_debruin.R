f6e2ee7d20b0930bd6e9acc8d590ebfa206f81f3
Kevin Koehler <kevin.koehler@santannapisa.it>
2025-05-16 16:12:14 +0200

    Update code_debruin.R

--- code_debruin.R
+++ code_debruin.R
@@ -5,3 +5,21 @@ data <- data %>%
     cbcount == 2 ~ "2",
     cbcount >= 3 ~ "3+"
   ))
+
+row1 <- c(table(data$cbcat),
+          table(data$newcb),
+          sum(!is.na(data$attempt), na.rm=T))
+row2 <- c(table(data$cbcat[data$attempt==0]),
+          table(data$newcb[data$attempt==0]),
+          sum(data$attempt==0, na.rm=T))
+row3 <- paste0(round(((row1-row2)/row1)*100,0),"%")
+row4 <- c(table(data$cbcat[data$attempt==1]),
+          table(data$newcb[data$attempt==1]),
+          sum(data$attempt==1, na.rm=T))
+row5 <- c(table(data$cbcat[data$success==0]),
+          table(data$newcb[data$success==0]),
+          sum(data$success==0, na.rm=T))
+row6 <- c(table(data$cbcat[data$success==1]),
+          table(data$newcb[data$success==1]),
+          sum(data$success==1, na.rm=T))
+row7 <- paste0(round(((row5-row6)/row5)*100,0),"%")
