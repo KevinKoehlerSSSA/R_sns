49c8c25fd50ac9f76fd11a6463af35dcda335706
Kevin Koehler <kevin.koehler@santannapisa.it>
2025-05-20 12:18:27 +0200

    Update Exercise.R

--- Exercise.R
+++ Exercise.R
@@ -4,3 +4,9 @@ html <- read_html(url)
 us_table <- html %>% html_elements("table.wikitable") %>%
   html_table() %>%
   as.data.frame()
+
+us_table <- us_table %>%
+  select(Name.birth.death., Term.16., Party.b..17..1) %>%
+  rename(name=Name.birth.death.,
+         term=Term.16.,
+         party=Party.b..17..1) 
