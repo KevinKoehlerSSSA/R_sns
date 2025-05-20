23c9c061207b48ba81ea84ffee2f1ba15474bfa0
Kevin Koehler <kevin.koehler@santannapisa.it>
2025-05-20 12:39:59 +0200

    Update Exercise.R

--- Exercise.R
+++ Exercise.R
@@ -1,12 +1,35 @@
 url <- "https://en.wikipedia.org/wiki/List_of_presidents_of_the_United_States"
 html <- read_html(url)
 
+
+
 us_table <- html %>% html_elements("table.wikitable") %>%
   html_table() %>%
   as.data.frame()
 
+names(us_table) <- c("var1",
+                     "var2",
+                     "var3",
+                     "var4",
+                     "var5",
+                     "var6",
+                     "var7",
+                     "var8")
+
 us_table <- us_table %>%
-  select(Name.birth.death., Term.16., Party.b..17..1) %>%
-  rename(name=Name.birth.death.,
-         term=Term.16.,
-         party=Party.b..17..1) 
+  select(var3, var4, var6) %>%
+  rename(name=var3,
+         term=var4,
+         party=var6) 
+
+
+str_extract("George Washington(1732–1799)[19]","")
+
+## Regex 1: Everything until you hit an opening bracket: .+(?=\\()
+## Regex 2: Everything until you hat a dash .+(?=–)
+## Regex 3: Everything following a dash (?<=–).+
+
+us_table <- us_table %>%
+  mutate(name=str_extract(name,".+(?=\\()"),
+         start_term=str_extract(term,".+(?=–)"),
+         end_term=str_extract(term,"(?<=–).+"))
